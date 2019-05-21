e57c900933e30dbefe94267e4f0b3a88cc3b5d47
lukasstreibl <50740794+lukasstreibl@users.noreply.github.com>
2019-05-21 14:53:58 +0200

    Update puzzle.m

--- puzzle.m
+++ puzzle.m
@@ -19,7 +19,9 @@ classdef puzzle < handle
             elseif(p1.turn==1)
                 p1.turn=0;
             end
-            p.stones(4)=s(16);
+            p.stones(4)=s(15);
+            p5=s(15);
+            if(p1
             p2=s(7);
             p.stones(1)=s(7);
             if(p2.turn==0)
@@ -28,7 +30,15 @@ classdef puzzle < handle
                 p2.turn=0;
             end
             p.stones(7)=s(24);
+            p3=s(24)
+            if (p3.turn==0)
+                p3.turn=1;
+            elseif(p3.turn==1)
+                p3.turn=0;
+            end       
             p.stones(10)=s(4);
+            p4=s(4);
+            
             p.stones(15)=s(21);
             p.stones(18)=s(1);
             p.stones(21)=s(10);

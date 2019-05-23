44d6bbf6cc84b8d284189c314ff4ccb017365e43
einlzweip <50743933+einlzweip@users.noreply.github.com>
2019-05-23 09:14:06 +0200

    Update puzzle.m

--- puzzle.m
+++ puzzle.m
@@ -121,7 +121,8 @@ classdef puzzle < handle
         if(s(q1,2) == 0)
             p.stones(q1,2) = 1;
         elseif(s(q1,2) == 1)
-        p.stones(q1,2) = 0;
+            p.stones(q1,2) = 0;
+        endif;    
         eq4=find(position == 6);
         p.stones(q4,1) = 8;
         if(s(q4,2) == 0)
@@ -214,9 +215,86 @@ classdef puzzle < handle
         elseif(s(q8,2) == 2 & (any(p.Rot == q8) | any(p.Orange == q8)))
             p.stones(q8,2) = 1;       
         end
+       
+    functionVp(p)
+        s = p.stones;
+        position = s(:,1); 
         
+        p1=find(position==1);
+        p.stones(p1,1)=18;
+        if(s(p1,2)==0)
+            p.stones(p1,2)=2;
+        elseif(s(p1,2)==2)
+            p.stones(p1,2)=0;
+        endif;
         
+        p2=find(position==2);
+        p.stones(p2,1)=10;
+        if(s(p2,2)==0)
+            p.stones(p2,2)=1;
+        elseif(s(p2,2)==1 & (any(p.Weiß==p2)|any(p.Gelb==p2)))
+            p.stones(p2,2)=2;
+        elseif(s(p2,2)==1)
+            p.stones(p2,2)=0;
+        elseif((any(p.Weiß==p2)|any(p.Gelb==p2)&(any(p.Grün==p2)|any(p.Blau==p2)))
+            p.stones(p2,2)=1;
         
+        p3=find(position==3);
+        p.stones(p3,1)=1;
+        if(s(p3,2)==0)
+            p.stones(p3,2)=2;
+        elseif(s(p3,2)==2)
+            p.stones(p3,2)=0;
+        endif;
+        
+        p4=find(position==10);
+        p.stones(p4,1)=19;
+        if(s(p4,2)==0)
+            p.stones(p4,2)=1;
+        elseif(s(p4,2)==1 & (any(p.Grün==p4)|any(p.Blau==p4)))
+            p.stones(p4,2)=0;
+        elseif(s(p4,2)==1)
+            p.stones(p4,2)=2;
+        elseif((any(p.Weiß==p4)|any(p.Gelb==p4)&(any(p.Orange==p4)|any(p.Rot==p4))
+            p.stones(p4,2)=1;
+            
+        p5=find(position==12);
+        p.stones(p5,1)=2;
+        if(s(p5,2)==0)
+            p.stones(p5,2)=1;
+        elseif(s(p5,2)==1 & (any(p.Grün==p5)|any(p.Blau==p5)))
+            p.stones(p5,2)=0;
+        elseif(s(p5,2)==1)
+            p.stones(p5,2)=2;
+        elseif((any(p.Weiß==p5)|any(p.Gelb==p5)&(any(p.Orange==p5)|any(p.Rot==p5))
+            p.stones(p5,2)=1;
+        
+        p6=find(position==18);
+        p.stones(p6,1)=20;
+        if(s(p6,2)==0)
+            p.stones(p6,2)=2;
+        elseif(s(p6,2)==2)
+            p.stones(p6,2)=0;
+        endif;
+        
+        p7=find(position==19);
+        p.stones(p7,1)=12;
+        if(s(p7,2)==0)
+            p.stones(p7,2)=1;
+        elseif(s(p7,2)==1 & (any(p.Weiß==p7)|any(p.Gelb==p7)))
+            p.stones(p7,2)=2;
+        elseif(s(p7,2)==1)
+            p.stones(p7,2)=0;
+        elseif((any(p.Weiß==p7)|any(p.Gelb==p7)&(any(p.Grün==p7)|any(p.Blau==p7)))
+            p.stones(p7,2)=1;
+        
+        p8=find(position==20);
+        p.stones(p8,1)=3;
+        if(s(p8,2)==0)
+            p.stones(p8,2)=2;
+        elseif(s(p8,2)==2)
+            p.stones(p8,2)=0;
+        endif;
     end
     
 end

a27d41a2ef2785e0b17fb74a3eebfffdbd06311d
lukasstreibl <50740794+lukasstreibl@users.noreply.github.com>
2019-05-24 10:43:36 +0200

    Update Plot.m

--- Plot.m
+++ Plot.m
@@ -1,6 +1,6 @@
 vert = [0 0 0;1 0 0;1 1 0;0 1 0;0 0 1;1 0 1;1 1 1;0 1 1];
 fac = [1 2 6 5;2 3 7 6;3 4 8 7;4 1 5 8;1 2 3 4;5 6 7 8];
-C = [0 1 0; 1 0 0; 0 0 1; 1 0.5 0; 1 1 1; 1 1 0];
+C = [1 0.5 0; 1 0 0; 0 0 1; 0 1 0; 1 1 1; 1 1 0];
 patch('Vertices',vert,'Faces',fac,'FaceVertexCData',C,'FaceColor','flat')
 view(3)
 axis vis3d

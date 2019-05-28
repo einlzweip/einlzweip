c739e41b551257afa1c7dc4ff0c24370b6657fff
Rob11223 <50744044+Rob11223@users.noreply.github.com>
2019-05-28 13:10:49 +0200

    Update Plot.m

--- Plot.m
+++ Plot.m
@@ -1,6 +1,9 @@
 vert = [0 0 0;1 0 0;1 1 0;0 1 0;0 0 1;1 0 1;1 1 1;0 1 1];
 fac = [1 2 6 5;2 3 7 6;3 4 8 7;4 1 5 8;1 2 3 4;5 6 7 8];
 C = [1 0.5 0; 0 0 1; 1 0 0; 0 1 0; 1 1 1; 1 1 0];
+%    1 orange; 2 blau; 3 rot; 4 gruen; 5 weiss; 6 gelb
+Color_stones = [3 4 6; 4 6 0; 4 6 1; 3 6 0; 6 0 0; 1 6 0; 2 3 6; 2 6 0; 4 2 6; 4 3 0; 4 0 0; 4 1 0; 3 0 0; 5 0 0; 3 2 0; 2 0 0;
+1 2 0; 3 4 5; 4 5 0; 4 5 1; 3 5 0; 5 0 0; 1 5 0; 3 2 5; 5 2 0; 1 2 5]
 patch('Vertices',vert,'Faces',fac,'FaceVertexCData',C,'FaceColor','flat')
 view(3)
 axis vis3d

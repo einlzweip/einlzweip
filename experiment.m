61aa9dd0b120a1a33f3887961bcc5e7e7119bd0b
einlzweip <50743933+einlzweip@users.noreply.github.com>
2019-06-20 19:30:29 +0200

    Update experiment.m

--- experiment.m
+++ experiment.m
@@ -1,15 +1,20 @@
 function experiment(a)
-A=zeros(26,2);
-A(:,1)=1:26;
-p=puzzle(A);
-for i=1:a
-    Rp(p);
-    Op(p);
-    Op(p);
-    Un(p);
-    Hp(p);
-    Un(p);
-end
-plotcube(p.stones);
-p.stones
+    %Experiment, ob zu überprüfen, ob die Drehungen richtig funktionieren
+    %Bei experiment (1260) sollte der gelöste Würfel herauskommen, bei allen kleineren Argumenten ist es nicht der gelöste Würfel
+    
+    A=zeros(26,2);
+    A(:,1)=1:26;
+    p=puzzle(A);
+    %Drehungsfolge
+    for i=1:a
+        Rp(p);
+        Op(p);
+        Op(p);
+        Un(p);
+        Hp(p);
+        Un(p);
+    end
+    %Plot des resultierenden Würfels
+    plotcube(p.stones);
+    p.stones
 end
